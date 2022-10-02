fade7bbee6d10c5849fe503cdaadb8bbd28540fd
shalawkarim <shalaw@glaciermediadigital.ca>
2022-10-02 16:51:24 -0700

    changes

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,9 @@
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 import { Icon } from 'leaflet';
 import iconPath from '../images/map_pin.png';
-import MarkerClusterGroup from "react-leaflet-markercluster";
 import { useAtom } from 'jotai';
 import { dataAtom } from '../store/store';
 import JOB_DATA from '../store/JOB_DATA.json';
@@ -13,20 +12,24 @@ const Map = () => {
     const map_tiles = 'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png';
     const position = [51.505, -0.09];
     const isBrowser = typeof window !== "undefined"
+    const [data, setData] = useAtom(dataAtom)
+    useEffect(() => {
+        setData(JOB_DATA)
+    }, [])
 
 
-  return (
-    isBrowser ? <MapContainer center={position} zoom={5} scrollWheelZoom={false} style={{height: "100vh", width: "100vw"}}>
+  return isBrowser ? (
+      <MapContainer center={position} zoom={5} scrollWheelZoom={false} style={{height: "100vh", width: "100vw"}}>
             <TileLayer
             url={map_tiles}
             />
-                {JOB_DATA?.map(item => {
+                {data?.map(item => {
                             return <Marker 
                             position={[item?.latitude, item?.longitude]}
                             key={item?.id}
                             icon = { new Icon({
                                 iconUrl: iconPath,
-                                iconSize: [60, 62],
+                                iconSize: [40, 40],
                                 iconAnchor: [20, 32],
                                 })} 
                             >
@@ -35,8 +38,8 @@ const Map = () => {
                         </Popup>
                         </Marker>
                 })}
-        </MapContainer> : 'Not defined'
-  )
+        </MapContainer> 
+  ) : <p>Not defined</p>
 }
 
 export default Map
